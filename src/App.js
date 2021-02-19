64771cecb8925220be8da04d2a53ae9a7b5a1151
Laurynas Juzva <laurynas.juzva@gmail.com>
2021-02-19 10:47:20 -0800

    almost done.

--- src/App.js
+++ src/App.js
@@ -4,8 +4,15 @@ import './App.css';
 
 
 class App extends React.Component {
-componentDidMount() {
-  console.log('loading...');
+
+insertGapiScript(){
+  const script = document.createElement('script')
+  script.src = 'https://apis.google.com/js/platform.js'
+  script.onload = () => {
+    this.initializeGoogleSignIn()
+  }
+}
+initializeGoogleSignIn(){
   window.gapi.load('auth2', () => {
     window.gapi.auth2.init({
       client_id: '135593514929-07lv33frtb3cose08i0ihplsi7a1jnn1.apps.googleusercontent.com'
@@ -23,9 +30,18 @@ componentDidMount() {
   })
 }
 
+
+
+componentDidMount() {
+  console.log('loading...');
+
+    this.insertGapiScript();
+
+}
+
   render() {
     return (
-      <div className="App">
+      <div className="App">  
        <h1>Google Login</h1>
     <div id='loginButton'>Sign in with Google</div>
       </div>
