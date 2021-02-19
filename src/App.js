d109caf9d00b5fc0498402109a92d72689cdc226
Laurynas Juzva <laurynas.juzva@gmail.com>
2021-02-19 10:13:36 -0800

    started coding

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-  <h1>Google Login</h1>
-  <button>Sign in with Google</button>
-    </div>
-  );
+
+
+class App extends React.Component {
+componentDidMount() {
+  console.log('loading...');
+  window.gapi.load('auth2', () => {
+    window.gapi.auth2.init({
+      client_id: '135593514929-07lv33frtb3cose08i0ihplsi7a1jnn1.apps.googleusercontent.com'
+    })
+    console.log('Api inited')
+  })
 }
+
+  render() {
+    return (
+      <div className="App">
+       <h1>Google Login</h1>
+    <button>Sign in with Google</button>
+      </div>
+    );
+  }
+  }
+
+
 
 export default App;
